8ef15b0fe43459e0f44058b2ad03bf90a8ef3fae
Emerson N <gammanolasco7@gmail.com>
2023-11-21 17:00:08 -0600

    style: add initial theme colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const colors = {
+  primary: { 2: "#EBA59E", 4: "#DA584B" },
+  secondary: { 4: "#70B252" },
+  tertiary: { 4: "#E5B454" },
+  neutral: {
+    2: "#94979A",
+    3: "#393D41",
+    4: "#2C2F33",
+    5: "#222528",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
