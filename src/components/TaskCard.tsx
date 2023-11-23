bed9ba6d28a3c36e48a85521ac75a170f1608641
Emerson N <gammanolasco7@gmail.com>
2023-11-23 01:32:06 -0600

    style(TaskCard): add three dot menu

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,18 @@
-import { Avatar, Box, Flex, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+  Avatar,
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import Chip from "./Chip";
+import { DeleteIcon, EditIcon, TimeIcon } from "@chakra-ui/icons";
+import IconDots from "../assets/icons/IconDots";
 
 const TaskCard = () => {
   return (
@@ -14,13 +27,43 @@ const TaskCard = () => {
     >
       <Flex justifyContent="space-between" pb={4}>
         <Text fontWeight={600}>Slack</Text>
-        <Box w={6} h={6} bg="neutral.300"></Box>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            size="sm"
+            transition="all 0.2s"
+            color="neutral.200"
+            aria-label="Options"
+            bg="neutral.400"
+            _hover={{ bg: "neutral.300" }}
+            _expanded={{ bg: "neutral.200", color: "neutral.400" }}
+            icon={<IconDots />}
+          />
+          <MenuList w="140px">
+            <MenuItem>
+              <EditIcon boxSize={5} />
+              <Text size="sm" ml={2}>
+                Edit
+              </Text>
+            </MenuItem>
+            <MenuItem>
+              <DeleteIcon boxSize={5} />
+              <Text size="sm" ml={2}>
+                Delete
+              </Text>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
       <Flex justifyContent="space-between">
         <Text fontSize="small" fontWeight={600}>
           4 points
         </Text>
-        <Chip icon={<Box />} label="Tomorrow" color="neutral.light" />
+        <Chip
+          icon={<TimeIcon boxSize={4} />}
+          label="Tomorrow"
+          color="neutral.light"
+        />
       </Flex>
       <Wrap mt={4} spacing={"8px"} mb={4}>
         <WrapItem>
