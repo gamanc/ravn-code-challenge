3a398b654ffca31717adb831cd07b5cfbcc95976
Emerson N <gammanolasco7@gmail.com>
2023-11-23 01:48:47 -0600

    style: update theme settings

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,4 +1,8 @@
-const chakraThemeConfig = {
+import { extendTheme } from "@chakra-ui/react";
+import type { StyleFunctionProps } from "@chakra-ui/styled-system";
+import { mode } from "@chakra-ui/theme-tools";
+
+export default extendTheme({
   colors: {
     primary: { 100: "#3D3336", 200: "#EBA59E", 400: "#DA584B" },
     secondary: { 100: "#333D37", 400: "#70B252" },
@@ -16,20 +20,19 @@ const chakraThemeConfig = {
     },
   },
   styles: {
-    global: {
+    global: (props: StyleFunctionProps) => ({
       html: {
         height: "100%",
       },
       body: {
         minHeight: "100vh",
-        bg: "neutral.500",
-        color: "white",
+        bg: mode("white", "neutral.500")(props),
+        color: mode("neutral.500", "white")(props),
       },
-    },
+    }),
   },
   config: {
     initialColorMode: "dark",
+    useSystemColorMode: false,
   },
-};
-
-export default chakraThemeConfig;
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import chakraThemeConfig from "./constants/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ColorModeScript
+      initialColorMode={chakraThemeConfig.config.initialColorMode}
+    />
     <App />
   </React.StrictMode>
 );
