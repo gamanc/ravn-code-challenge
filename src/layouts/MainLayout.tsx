0a7b10cea03d0b1322175e4f11c3ade09ceaf3d2
Emerson N <gammanolasco7@gmail.com>
2023-11-22 23:16:53 -0600

    feat: display sidebar component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,22 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Container, Grid, GridItem } from "@chakra-ui/react";
+import Sidebar from "../components/Sidebar";
 
 const MainLayout = () => {
   return (
     <Container
       maxWidth={"7xl"}
-      pt={10}
+      py={6}
       bg={"neutral.500"}
       color="white"
-      maxHeight="100vh"
+      maxHeight={{ lg: "100vh" }}
       overflowY="hidden"
     >
-      <Grid templateColumns="repeat(6, 1fr)">
-        <GridItem as="aside" colSpan={1} minWidth="232px">
-          <span>Sidebar</span>
+      <Grid templateColumns="repeat(6, 1fr)" gap={10}>
+        <GridItem
+          as="aside"
+          colSpan={{ base: 6, lg: 2, xl: 1 }}
+          minWidth="232px"
+          minHeight={{ lg: "100%" }}
+        >
+          <Sidebar />
         </GridItem>
-        <GridItem as="main" colSpan={5}>
+        <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }}>
           <Navbar />
           <Outlet />
         </GridItem>
