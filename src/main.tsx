06535ecd64bdb0329e53e8a9da71d00a0eb822b5
Emerson N <gammanolasco7@gmail.com>
2023-11-24 01:17:34 -0600

    chore(main.tsx): reorder imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ColorModeScript } from "@chakra-ui/react";
+import chakraThemeConfig from "./config/theme.ts";
+
 import App from "./App.tsx";
-import chakraThemeConfig from "./constants/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
