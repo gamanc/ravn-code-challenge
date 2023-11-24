2cf9fee2369838b96da8f67db2d735a5a43c0977
Emerson N <gammanolasco7@gmail.com>
2023-11-24 01:18:55 -0600

    feat: create initial graphql queries

---
+++ src/querys/taskQuerys.ts
@@ -0,0 +1,27 @@
+import { graphql } from "../gql";
+
+const TASKS_QUERY = graphql(`
+  query GetAllTasks($input: FilterTaskInput!) {
+    tasks(input: $input) {
+      id
+      position
+      pointEstimate
+      name
+      dueDate
+      status
+      tags
+      assignee {
+        id
+        fullName
+        avatar
+      }
+      creator {
+        id
+        fullName
+        avatar
+      }
+    }
+  }
+`);
+
+export { TASKS_QUERY };
