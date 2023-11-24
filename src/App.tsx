da4726f11ff8615eb6624444d6163449d15707eb
Emerson N <gammanolasco7@gmail.com>
2023-11-24 01:16:40 -0600

    chore: fix theme import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloProvider } from "@apollo/client";
 
 import { client } from "./config/apollo";
-import theme from "./constants/theme";
+import theme from "./config/theme";
 import { router } from "./config/router";
 import { RouterProvider } from "react-router-dom";
 
