8316a72dbbbf8802bb0cc445d326c29d7bce43f0
Emerson N <gammanolasco7@gmail.com>
2023-11-24 23:19:45 -0600

    chore: add react-hook-form and sassy-datepicker dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { ColorModeScript } from "@chakra-ui/react";
 import chakraThemeConfig from "./config/theme.ts";
 
+import "./../node_modules/sassy-datepicker/dist/styles.css";
+import "./assets/styles/sassy-datepicker.css";
+
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
